9b9a73011045b5fd8561b2337d893e49e3f141af
Toymaker <Toymaker.unity@gmail.com>
2026-06-22 23:39:11 +0300

    Test2

--- Assets/Script/UnityTimeService.cs
+++ Assets/Script/UnityTimeService.cs
@@ -4,7 +4,7 @@ namespace Script
 {
     public class UnityTimeService : ITimeService
     {
-        public int Test;
+        public int Test2;
         public float FrameTime => Time.time;
         public float DeltaTime => Time.deltaTime;
     }
